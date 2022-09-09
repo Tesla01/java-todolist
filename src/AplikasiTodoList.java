1acbb3707b8fb7c1cd418efda5c1c715ae8b98a7
Arif Sulaiman <arif.sulaiman720@gmail.com>
2022-09-09 14:27:11 +0700

    Initial Commit

---
+++ src/AplikasiTodoList.java
@@ -0,0 +1,32 @@
+public class AplikasiTodoList {
+
+    public static String[] model = new String[10];
+
+    public static void main(String[] args) {
+
+    }
+
+    // Menampilkan Todolist
+    public static void showTodoList() {
+    }
+
+    // Menambah Todo ke List
+    public static void addTodoList() {
+    }
+
+    // Menghapus Todo dari List
+    public static void removeTodoList() {
+    }
+
+    // Menampilkan view Todolist
+    public static void viewTodoList() {
+    }
+
+    // Menampilkan view menambahkan Todolist
+    public static void viewAddTodoList() {
+    }
+
+    // Menampilkan view menghapus Todolist
+    public static void viewRemoveTodoList() {
+    }
+}
