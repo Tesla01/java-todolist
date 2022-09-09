4e1da0360eacd724183fd21616a3b0ac5cf93c2b
Arif Sulaiman <arif.sulaiman720@gmail.com>
2022-09-09 14:41:13 +0700

    Add Test

--- src/AplikasiTodoList.java
+++ src/AplikasiTodoList.java
@@ -3,14 +3,25 @@ public class AplikasiTodoList {
     public static String[] model = new String[10];
 
     public static void main(String[] args) {
-
+        testShowTodoList();
     }
 
     // Menampilkan Todolist
     public static void showTodoList() {
         for (var i = 0; i < model.length; i++) {
-            String todo = model[i];
+            var todo = model[i];
+            var no = i + 1;
+
+            if (todo != null) {
+                System.out.println(no + ". " + todo);
+            }
         }
+    }
+
+    public static void testShowTodoList() {
+        model[0] = "Belajar java dasar";
+        model[1] = "Studi kasus java dasar";
+        showTodoList();
     }
 
     // Menambah Todo ke List
